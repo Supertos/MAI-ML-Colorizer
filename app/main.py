13750fcb26523462d8564492abd95329ff14b454
Bachurin Nikolay <101479452+rox0-0@users.noreply.github.com>
2025-02-22 20:31:58 +0300

    Creating FastApi app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,21 @@
-
-
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles 
 from app.routes.image_routes import router as image_router
 from fastapi.middleware.cors import CORSMiddleware
 
 def create_app() -> FastAPI:
-    #Создание приложения
+    """
+    Функция для создания приложения,
+    чтобы в будущем при необходимости передавать настройки.
+    """
     app = FastAPI(title="Colorizer Project")
-    
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["http://localhost:3000"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.mount("/static", StaticFiles(directory="app/static"), name="static")
     app.include_router(image_router)
     return app
