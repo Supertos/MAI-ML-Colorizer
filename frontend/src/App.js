65f539420df8d2cc9eb766cce370609b409645e2
mnlyuzina <mnlyuzina@yandex.ru>
2025-03-25 20:13:23 +0300

    adding base architecture

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+// import PhotoUpload from './components/PhotoUpload';
+// import PhotoGrid from './components/PhotoGrid';
+// import About from './components/About';
+//import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1 className="header-title">Has-Been</h1>
+        <p className="header-subtitle">Восстановление цвета</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
